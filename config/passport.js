a3c31548280d56274ec230a8e9d7bd3ce7d643f6
Howard <how2945ard@gmail.com>
2014-06-11 20:08:25 +0800

    heroku

--- config/passport.js
+++ config/passport.js
@@ -2,8 +2,8 @@ var passport = require('passport');
 var FacebookStrategy = require('passport-facebook').Strategy;
 
 passport.use(new FacebookStrategy({
-  clientID: '1430907223846624',
-  clientSecret: '35fb93ab34f993c28650c2cd9aa2ee2b',
+  clientID: process.env.FB_APP_ID,
+  clientSecret: process.env.FB_APP_SECRET,
   callbackURL: "/fbcb"
 }, function(accessToken, refreshToken, profile, done){
   // Passport profile:
